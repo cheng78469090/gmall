0e23cda56a0169018fcc82e3aff951630d2f8d57
宋金城 <1156920605@qq.com>
2019-12-31 16:54:41 +0800

    逆向工程完成

---
+++ gmall-ums/src/main/java/com/atguigu/gmall/ums/dao/MemberLoginLogDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.ums.dao;
+
+import com.atguigu.gmall.ums.entity.MemberLoginLogEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 会员登录记录
+ * 
+ * @author lixianfeng
+ * @email lxf@atguigu.com
+ * @date 2019-12-31 16:47:34
+ */
+@Mapper
+public interface MemberLoginLogDao extends BaseMapper<MemberLoginLogEntity> {
+	
+}
