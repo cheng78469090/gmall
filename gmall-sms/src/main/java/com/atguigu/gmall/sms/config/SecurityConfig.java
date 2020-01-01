c90ffe3f9263149df5e3bc353d41faf22fb43349
宋金城 <1156920605@qq.com>
2020-01-01 20:00:42 +0800

    2020.1.1配置信息修改完成

---
+++ gmall-sms/src/main/java/com/atguigu/gmall/sms/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.sms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Auther: 宋金城
+ * @Date: 2020/1/1 14:34
+ * @Description:权限管理配置文件
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+        // 禁用掉csrf
+        http.csrf().disable();
+    }
+
+}
